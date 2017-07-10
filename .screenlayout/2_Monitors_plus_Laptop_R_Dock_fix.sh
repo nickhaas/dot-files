d9f312d3a8072e2c35bd95adc3e8e9166370eb17
nickhaas <nickias.haas@protonmail.ch>
2017-07-10 10:45:07 +0200

    screenfiles added

---
+++ .screenlayout/2_Monitors_plus_Laptop_R_Dock_fix.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+xrandr --newmode "1920x1200"
+xrandr --addmode DP1-2 1920x1200
+xrandr --addmode DP1-1 1920x1200
+sleep 5
+xrandr --output VIRTUAL1 --off 
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 3840x0 --rotate normal 
+xrandr --output DP1 --off 
+xrandr --output HDMI2 --off 
+xrandr --output HDMI1 --off 
+xrandr --output DP1-3 --off 
+xrandr --output DP1-2 --mode 1920x1200 --pos 1920x0 --rotate normal 
+xrandr --output DP1-1 --mode 1920x1200 --pos 0x0 --rotate normal
+nitrogen --restore
